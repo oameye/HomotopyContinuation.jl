9539feea88b10b2d9e7479a31dedfb8478041bc6
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-13 15:47:09 +0200

    Move the ambient-coordinate tests into witness_set_test.jl

--- test/witness_set_test.jl
+++ test/witness_set_test.jl
@@ -416,3 +416,23 @@ rand_poly(vars, d; homogeneous = false) = rand_poly(Float64, vars, d; homogeneou
     end
 
 end
+
+@testset "Witness-set ambient coordinates" begin
+    @polyvar x y z
+    F = System([x]; variables = [x, y])
+    G = System([y]; variables = [x, y])
+    H = System([z]; variables = [x, z])
+    R = System([x]; variables = [y, x])
+
+    @test isnothing(HomotopyContinuationNext._check_same_ambient_variables(F, G))
+    @test_throws ArgumentError HomotopyContinuationNext._check_same_ambient_variables(F, H)
+    @test_throws ArgumentError HomotopyContinuationNext._check_same_ambient_variables(F, R)
+
+    L = LinearSubspace(zeros(ComplexF64, 0, 2), ComplexF64[])
+    empty_points = Vector{Vector{ComplexF64}}()
+    W = WitnessSet(F, L, copy(empty_points))
+    WH = WitnessSet(H, L, copy(empty_points))
+    @test_throws ArgumentError intersect(
+        W, WH, Intersection(; show_progress = false), Serial(),
+    )
+end
