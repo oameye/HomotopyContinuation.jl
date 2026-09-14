d927515e8215d7d139395a2c8d2e73be37dd4e39
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-14 11:29:38 +0200

    ci: enforce stability during core tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,14 @@
+using Preferences: set_preferences!
+
+# DispatchDoctor is a production no-op by default. Tests turn the package-wide
+# stability contract into a hard error before HomotopyContinuation is loaded.
+set_preferences!(
+    "HomotopyContinuation",
+    "dispatch_doctor_mode" => "error",
+    "dispatch_doctor_codegen_level" => "min";
+    force = true,
+)
+
 using HomotopyContinuation
 using ParallelTestRunner: ParallelTestRunner, find_tests
 
